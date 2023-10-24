7272060810a272d4a17a851392bd904208f2937d
MasonQiao <119145677+MasonQiao@users.noreply.github.com>
2023-10-24 09:23:01 -0500

    Update bovine_genomics.cpp

--- bovine_genomics.cpp
+++ bovine_genomics.cpp
@@ -1,5 +1,3 @@
-// Source: https://usaco.guide/general/io
-
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -15,37 +13,46 @@ int main() {
 		cin >> cows[i];
 	}
 	for (int i = 0; i < m; i++) {
-		for(int j = 0; i < n; j++) {
-			if (cows[j][i] == 'a') {
+		sa = 0;
+		sc = 0;
+		sg = 0;
+		st = 0;
+		pa = 0;
+		pg = 0;
+		pc = 0;
+		pt = 0;
+		for(int j = 0; j < n; j++) {
+			if (cows[j][i] == 'A') {
 				sa = 1;
 			}
-			if (cows[j][i] == 'c') {
+			if (cows[j][i] == 'C') {
 				sc = 1;
 			}
-			if (cows[j][i] == 'g') {
+			if (cows[j][i] == 'G') {
 				sg = 1;
 			}
-			if (cows[j][i] == 't') {
+			if (cows[j][i] == 'T') {
 				st = 1;
 			}
 		}
-		for(int j = n; i < 2*n; j++) {
-			if (cows[j][i] == 'a') {
+		for(int j = n; j < 2*n; j++) {
+			if (cows[j][i] == 'A') {
 				pa = 1;
 			}
-			if (cows[j][i] == 'c') {
+			if (cows[j][i] == 'C') {
 				pc = 1;
 			}
-			if (cows[j][i] == 'g') {
+			if (cows[j][i] == 'G') {
 				pg = 1;
 			}
-			if (cows[j][i] == 't') {
+			if (cows[j][i] == 'T') {
 				pt = 1;
 			}
 		}
-		if (sa*pa == 0 && sc*pc == 0 && sg*pg == 0 && st*pt == 0) {
+		if ((sa*pa == 0) && (sc*pc == 0) && (sg*pg == 0) && (st*pt == 0)) {
 			ans ++;
 		}
 	}
 	cout << ans;
+	
 }
